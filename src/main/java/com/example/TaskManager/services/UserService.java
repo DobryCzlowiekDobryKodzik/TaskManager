eca2b6d3d247dfe604ca362bb5f712cb541fca78
Piotr <pitarita15@gmail.com>
2026-05-07 08:14:28 +0200

    feat(UserService): add createUser method

--- src/main/java/com/example/TaskManager/services/UserService.java
+++ src/main/java/com/example/TaskManager/services/UserService.java
@@ -1,7 +1,9 @@
 package com.example.TaskManager.services;
 
+import com.example.TaskManager.dtos.CreateUserRequest;
 import com.example.TaskManager.dtos.UserResponse;
 import com.example.TaskManager.entities.User;
+import com.example.TaskManager.mappers.UserMapper;
 import com.example.TaskManager.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -14,11 +16,20 @@ import java.util.List;
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
+    //@PreAuthorize("hasRole('ADMIN')")
     public List<UserResponse> getAllUsers() {
         return userRepository.findAll()
                 .stream()
                 .map(users -> new UserResponse(users.getId(),users.getUsername(),users.getRole()))
                 .toList();
+    }
+    public UserResponse createUser(CreateUserRequest userRequest) {
+        var user = userMapper.toEntity(userRequest);
+        UserResponse userResponse = userMapper.toDto(user);
+        userRepository.save(user);
+        return userResponse;
+
     }
 }
